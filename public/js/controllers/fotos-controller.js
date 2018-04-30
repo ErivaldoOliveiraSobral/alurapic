540fd0274b640ecec29f2ce9e58483c49e08e8a7
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-04-30 13:02:29 -0300

    2 aula (estudo)

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,13 +2,22 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 	
 	$scope.fotos = [];
-
 	$http.get('http://localhost:3000/v1/fotos')
 	.success(function(fotos) {
+		console.log(fotos);
 		$scope.fotos = fotos;
 	})
 	.error(function(erro) {
 		console.log(erro);
 	});
 
+	$scope.pedidos = [];
+	$http.get('https://alkimiasimplesassim.com.br/api/pedidos/')
+	.success(function(pedido) {
+		console.log(pedido);
+		$scope.pedidos = pedido;
+	})
+	.error(function(erro) {
+		console.log(erro);
+	});
 });
