1262fc00b034e89a4bf12c0c647a583646c97aaf
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-04-30 11:28:20 -0300

    1 aula

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,9 @@
+// Usando o modulo criado em main.js
+angular.module('alurapic').controller('FotosController', function($scope) {
+	
+	$scope.foto = {
+		titulo:"Leão",
+		url:"https://i0.wp.com/www.respingosdagraca.com.br/wp-content/uploads/2017/08/leao-660.jpg?fit=660%2C413&ssl=1"
+	};
+
+});
